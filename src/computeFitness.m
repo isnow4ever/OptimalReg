dd5982dee13a4947646793cb57f77f5cd4bfa93f
isnow4ever <gaoyongzhuo@yeah.net>
2018-07-02 11:15:18 +0800

    fix bugs

--- src/computeFitness.m
+++ src/computeFitness.m
@@ -1,19 +1,27 @@
-function fitness = computeFitness(theta,phi,psi,a,b,c)
+function fitness = computeFitness(X)
 %COMPUTEFITNESS 此处显示有关此函数的摘要
 %   此处显示详细说明
-    load '../data/data_with_normals.txt';
-    load '../data/model_with_normals.txt'
+    load 'data_with_normals.txt';
+    load 'model_with_normals.txt'
     cloud_model = model_with_normals(:,1:3);
     cloud_data = data_with_normals(:,1:3);
     normals_model = model_with_normals(:,4:6);
     normals_data = data_with_normals(:,4:6);
+    H = [0.01 0.1 0.01 20 20 0.5];
+    X = H'*X;
+    theta = X(1);
+    phi = X(2);
+    psi = X(3);
+    a = X(4);
+    b = X(5);
+    c = X(6);
     R = angle2dcm(theta,phi,psi);
     T = [a b c];
-    cloud_data = R*cloud_data+T;
+    cloud_data = (R*cloud_data'+T')';
     alpha = 0.2;
     beta = 0.8;
     probability = 0.7;
-    enveloped = estimateEveloped(probability, cloud_model, cloud_data);
+    enveloped = estimateEnveloped(probability, cloud_model, cloud_data);
     if enveloped == true
         datum_model = computeDatumCoefficients(cloud_model);
         datum_data = computeDatumCoefficients(cloud_data);
@@ -27,7 +35,7 @@ function fitness = computeFitness(theta,phi,psi,a,b,c)
 		sigmoid_v = 1 / (1 + sqrt(dist_variance));
 		fitness = alpha * sigmoid_e + beta * sigmoid_v;
     else
-        fitness = 0.0;
+        fitness = 1.0;
     end
 end
 
--- src/estimateEnveloped.m
+++ src/estimateEnveloped.m
@@ -8,7 +8,7 @@ function [eveloped,dist] = estimateEnveloped(probability, cloud_model, cloud_dat
     total_count = 0;
     dist = [];
     for i=1:layers_num
-        [points_on_planes_m, points_on_planes_d] = estimatePointsBySlicing(slicing_planes(i), cloud_model, cloud_data, 1.0);
+        [points_on_planes_m, points_on_planes_d] = estimatePointsBySlicing(slicing_planes(i,:), cloud_model, cloud_data, 1.0);
         pt_on_planes_m = zeros(size(points_on_planes_m,2),3);
         pt_on_planes_d = zeros(size(points_on_planes_d,2),3);
         for j=1:size(points_on_planes_m,2)
@@ -21,7 +21,7 @@ function [eveloped,dist] = estimateEnveloped(probability, cloud_model, cloud_dat
         total_count = total_count + size(points_on_planes_m,2);
 
         for k=1:size(points_on_planes_d,2)
-            pt_on_planes_d(k) = [points_on_planes_d(1),points_on_planes_d(2),0.0];
+            pt_on_planes_d(k,:) = [points_on_planes_d(1),points_on_planes_d(2),0.0];
         end
         idx = knnsearch(pt_on_planes_d,pt_on_planes_m);
         dist = [dist sqrt((pt_on_planes_d(idx,1) - pt_on_planes_m(:,1)).^2 + (pt_on_planes_d(idx,2) - pt_on_planes_m(:,2)).^2 + (pt_on_planes_d(idx,3) - pt_on_planes_m(:,3)).^2)];
